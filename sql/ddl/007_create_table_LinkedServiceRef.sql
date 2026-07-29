640c87ade0fba2e9066c601cfb1c2613a26c4189
Saikat Bhattacharjee <bhattacharjee.saikat3@gmail.com>
2026-07-29 11:47:52 +1000

    Update 007_create_table_LinkedServiceRef.sql

--- sql/ddl/007_create_table_LinkedServiceRef.sql
+++ sql/ddl/007_create_table_LinkedServiceRef.sql
@@ -20,14 +20,14 @@ BEGIN
     CREATE TABLE EDP_Metadata.LinkedServiceRef (
         LinkedService_ID              INT             IDENTITY(1,1)  NOT NULL,
         LinkedServiceName             NVARCHAR(100)   NOT NULL,
-        LinkedServiceConnectionName   NVARCHAR(200)   NOT NULL,
+        LinkedServiceConnectionName   NVARCHAR(200)   NULL, -- Changed to NULL, allowed duplicates
         Source_ID                     INT             NOT NULL,
         Auth_ID                       INT             NOT NULL,
         LinkedServiceConnectionString NVARCHAR(MAX)   NOT NULL,
 
         CONSTRAINT PK_LinkedServiceRef               PRIMARY KEY CLUSTERED (LinkedService_ID),
-        CONSTRAINT UQ_LinkedServiceRef_Name          UNIQUE (LinkedServiceName),
-        CONSTRAINT UQ_LinkedServiceRef_ConnName      UNIQUE (LinkedServiceConnectionName),
+        -- Removed UQ_LinkedServiceRef_Name to allow duplicate values
+        -- Removed UQ_LinkedServiceRef_ConnName to allow duplicate values
         CONSTRAINT FK_LinkedServiceRef_Source        FOREIGN KEY (Source_ID)
             REFERENCES EDP_Metadata.SourceRef  (Source_ID),
         CONSTRAINT FK_LinkedServiceRef_Auth          FOREIGN KEY (Auth_ID)
@@ -36,11 +36,25 @@ BEGIN
 END
 ELSE
 BEGIN
+    -- 1. Drop existing unique constraints if they exist in the live database
+    IF EXISTS (SELECT 1 FROM sys.objects WHERE name = 'UQ_LinkedServiceRef_Name' AND type = 'UQ')
+    BEGIN
+        ALTER TABLE EDP_Metadata.LinkedServiceRef DROP CONSTRAINT UQ_LinkedServiceRef_Name;
+    END
+
+    IF EXISTS (SELECT 1 FROM sys.objects WHERE name = 'UQ_LinkedServiceRef_ConnName' AND type = 'UQ')
+    BEGIN
+        ALTER TABLE EDP_Metadata.LinkedServiceRef DROP CONSTRAINT UQ_LinkedServiceRef_ConnName;
+    END
+
+    -- 2. Add the column as NULL if it does not exist
     IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID('EDP_Metadata.LinkedServiceRef') AND name = 'LinkedServiceConnectionName')
     BEGIN
         ALTER TABLE EDP_Metadata.LinkedServiceRef ADD LinkedServiceConnectionName NVARCHAR(200) NULL;
-        UPDATE EDP_Metadata.LinkedServiceRef SET LinkedServiceConnectionName = LinkedServiceName WHERE LinkedServiceConnectionName IS NULL;
-        ALTER TABLE EDP_Metadata.LinkedServiceRef ALTER COLUMN LinkedServiceConnectionName NVARCHAR(200) NOT NULL;
-        ALTER TABLE EDP_Metadata.LinkedServiceRef ADD CONSTRAINT UQ_LinkedServiceRef_ConnName UNIQUE (LinkedServiceConnectionName);
+    END
+    ELSE
+    BEGIN
+        -- If it exists but was previously NOT NULL, alter it to NULL
+        ALTER TABLE EDP_Metadata.LinkedServiceRef ALTER COLUMN LinkedServiceConnectionName NVARCHAR(200) NULL;
     END
 END
